b966f4baf9f2efa1cdab80cf0c362f62835ba293
danielpsrb <danielpsrb01@gmail.com>
2025-11-15 06:41:54 +0800

    first commit:auth service & error handler

--- apps/auth-service/src/main.ts
+++ apps/auth-service/src/main.ts
@@ -1,14 +1,23 @@
 import express from 'express';
-
-const host = process.env.HOST ?? 'localhost';
-const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+import cors from 'cors';
 
 const app = express();
+
+app.use(cors({
+  origin: ["http://localhost:3000"],
+  credentials: true,
+  allowedHeaders: ["Authorization", "Content-Type"],
+}));
 
 app.get('/', (req, res) => {
     res.send({ 'message': 'Hello API'});
 });
 
-app.listen(port, host, () => {
-    console.log(`[ ready ] http://${host}:${port}`);
+const port = process.env.PORT || 6001;
+const server = app.listen(port, () => {
+  console.log(`Auth service is running at http://localhost:${port}/api`);
+});
+
+server.on('error', (err) => {
+  console.log("Server Error:", err);
 });
