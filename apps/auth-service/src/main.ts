0c49e88618b9871a6da8960094c3bcc044fc55a7
danielpsrb <danielpsrb01@gmail.com>
2025-11-17 02:38:31 +0800

    feat: swagger document to entry point auth-service

--- apps/auth-service/src/main.ts
+++ apps/auth-service/src/main.ts
@@ -1,7 +1,10 @@
 import express from 'express';
 import cors from 'cors';
-import { errorMiddleware } from "../../../packages/error-handler/error-middleware";
+import { errorMiddleware } from "@packages/error-handler/error-middleware";
 import cookieParser = require("cookie-parser");
+import swaggerUi from 'swagger-ui-express';
+import router from './routes/auth.router';
+const swaggerDocument = require('./swagger-output.json');
 
 const app = express();
 
@@ -18,11 +21,21 @@ app.get('/', (req, res) => {
     res.send({ 'message': 'Hello API'});
 });
 
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.get('/docs-json', (req, res) => {
+    res.json(swaggerDocument);
+});
+
+// Routes
+app.use('/api', router);
+
 app.use(errorMiddleware)
 
 const port = process.env.PORT || 6001;
 const server = app.listen(port, () => {
   console.log(`Auth service is running at http://localhost:${port}/api`);
+  console.log(`API docs available at http://localhost:${port}/docs`);
 });
 
 server.on('error', (err) => {
